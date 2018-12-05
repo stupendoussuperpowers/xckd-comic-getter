444a1fe4e44f0e3fdeec4752ec95965c21963054
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2018-12-05 14:05:37 +0530

    Update xkcdcomic.py

--- xkcdcomic.py
+++ xkcdcomic.py
@@ -9,6 +9,7 @@ def comic_downloader(comic_number = 0):
     try:
         sauce = urllib.request.urlopen("https://xkcd.com/%d"%comic_number).read()
         soup = bs.BeautifulSoup(sauce,'html.parser')
+        #The actual comic is the second img on every page
         t = 0
         for i in soup.find_all('img'):
             if t == 1:
@@ -17,8 +18,10 @@ def comic_downloader(comic_number = 0):
             t+=1
         print ("Downloading Image:",comic_number, i.get('alt'))
         print(i.get('title'))
+        #replacing illegal characters in titles
         urllib.request.urlretrieve("https:"+img_url,i.get('alt').replace('?','Qm').replace('<','sl').replace('>','sr').replace('*','as').replace('~','tl').replace('#','hsh').replace('%','prc').replace('&','and').replace('{','cl').replace('}','cr').replace('\\','bs').replace(':','cln').replace('/','sl').replace('+','plus').replace('|','pipe').replace("'",'quotes').replace('"','quotes') + ".png")
     except:
+        #shouldn't happen hopefully
         print('Lmao Failed.')
         fail.append(comic_number)
 def comic_catalogue():
@@ -29,8 +32,8 @@ def comic_catalogue():
         soup = bs.BeautifulSoup(urllib.request.urlopen("https://xkcd.com/%d"%i).read(),'html.parser')
         catalogue[i] = soup.find_all('title')
     return catalogue
-
-for i in range(1525,1932):
+#Downloads all comics upto 1932, the latest one at the time of creation
+for i in range(1932):
     comic_downloader(i)
 
 
